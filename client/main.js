8c22857140087b3c66070bbd4d3e5322c100b848
Gaston Elhordoy <gaston.elhordoy@gmail.com>
2014-05-01 15:34:20 +0000

    Added security restriction, allowing only loggedin users to send messages

--- client/main.js
+++ client/main.js
@@ -1,23 +1,32 @@
-var chatStream = new Meteor.Stream('chat');
-
 Accounts.ui.config({
-  passwordSignupFields: 'USERNAME_ONLY'
+    passwordSignupFields: 'USERNAME_ONLY'
 });
 
 Template.hello.greeting = function () {
-  return "Welcome to meteor-buzzer!";
+    return "Welcome to meteor-buzzer!";
 };
 
+// TODO check for format support
+var index = 0;
+var sounds = ['truck.ogg', 'alarm.wav', 'ding.wav'];
+for (i = 0; i < sounds.length; i++) {
+    sounds[i] = new buzz.sound('/sounds/' + sounds[i], { preload: true });
+}
+
+var chatStream = new Meteor.Stream('chat');
+
 Template.hello.events({
-  'click input': function () {
-    chatStream.emit('selection', "Click!");
-    // console.log('me: ' + message);
+    'click input': function () {
+
+    chatStream.emit('selection', index);
+    index = (index + 1) % sounds.length;
 
     chatStream.on('selection', function(message) {
-        console.log('user: ' + message);
+        // FIXME check why the notification is being received multiple times
+        console.log('user: ' + message.user + ' | sound: ' + message.index);
         
-        var s = new buzz.sound('/sounds/truck.ogg');
-        s.play();
+        // TODO add validation for message.index
+        sounds[message.index].play();
     });
   }
 });
--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
+function selectionTransformationFilter(eventName, args) {
+    // TODO get username from userId
+    console.log('Event [%s] in TransformationFilter from user [%s]: %j', eventName, this.userId, args);
+    
+    if (eventName === 'selection') {
+        // TODO validate length
+        args[0] = { user: this.userId, index: args[0]};
+    }
+    return args;
+}
+
 Meteor.startup(function () {
     console.log('BUZZER started...');
 
     var chatStream = new Meteor.Stream('chat');
+    chatStream.addFilter(selectionTransformationFilter);
+    chatStream.permissions.write(function(eventName) {
+        return this.userId !== null;
+    }, false);
+    chatStream.permissions.read(function(eventName) {
+        return this.userId !== null;
+    }, false);
+  
     // console.log('Play: %j', Play);
 
     chatStream.on('selection', function(message) {
-        console.log('user: %s', message);
+        console.log('Selection: %j', message);
         //play.sound('../public/sounds/alarm.wav');
     });
 });
